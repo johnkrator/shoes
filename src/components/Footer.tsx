a7540c711b2c9728542ef98371c579e1996800ff
johnkrator <chukwuchidieberejohn@gmail.com>
2024-06-13 17:15:40 +0100

    Implement product page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div>
+        <div className="xl:px-20 md:px-10 sm:px-2 px-4 py-5 mb-5">
             Footer
         </div>
     );
---
+++ src/components/navbar/routeContants.ts
@@ -0,0 +1,14 @@
+export const headerLinks = [
+    {
+        label: "Home",
+        route: "/"
+    },
+    {
+        label: "Register",
+        route: "/register"
+    },
+    {
+        label: "Login",
+        route: "/login"
+    }
+];
