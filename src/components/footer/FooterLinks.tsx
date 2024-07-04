37229f4f857246bb1f53a98cdd76cb84d9bcd32f
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-04 05:36:56 +0100

    Fix footer

--- src/components/footer/FooterLinks.tsx
+++ src/components/footer/FooterLinks.tsx
@@ -3,7 +3,8 @@ import {BsTelephoneFill} from "react-icons/bs";
 
 const FooterLinks = () => {
     return (
-        <div className="grid md:grid-cols-4 grid-cols-2 lg:gap-0 md:gap-5 gap-11 lg:mt-0 mt-10 w-[60vw]">
+        <div
+            className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-0 gap-11 lg:mt-0 mt-10 w-[60vw]">
             <ul className="flex flex-col gap-3">
                 <h1 className="font-bold">My account</h1>
                 <li>Login/Register</li>
@@ -33,12 +34,12 @@ const FooterLinks = () => {
             <ul className="flex flex-col gap-3">
                 <h1 className="font-bold">Hotlines</h1>
                 <div className="flex items-center gap-1">
-                    <FaRegEnvelope/>
-                    <li>mail@trendyshoes.com</li>
+                    <FaRegEnvelope className="text-lg sm:text-xl lg:text-2xl flex-shrink-0"/>
+                    <li className="text-sm sm:text-base">mail@trendyshoes.com</li>
                 </div>
                 <div className="flex items-center gap-1">
-                    <BsTelephoneFill/>
-                    <li>+2349099204983</li>
+                    <BsTelephoneFill className="text-lg sm:text-xl lg:text-2xl flex-shrink-0"/>
+                    <li className="text-sm sm:text-base">+2349099204983</li>
                 </div>
             </ul>
         </div>
