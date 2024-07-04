b484551bff65ad5997095f67db0c6bb65be11f38
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-04 04:10:58 +0100

    Code refactor

--- src/components/CustomersReview.tsx
+++ src/components/CustomersReview.tsx
@@ -5,7 +5,7 @@ const CustomersReview = () => {
     return (
         <div className="bg-[linear-gradient(to_right,#fde6e6_50%,#f67018_50%)] w-[100%] py-10">
             <Container>
-                <div className="flex lg:flex-row flex-col md:items-center lg:gap-40 gap-5">
+                <div className="flex  flex-col  gap-5">
                     <h1 className="text-[40px] font-bold leading-10">
                         "WHAT OUR <br/> <span className="text-[#E0551B] whitespace-nowrap">CUSTOMER SAYS</span>"
                     </h1>
