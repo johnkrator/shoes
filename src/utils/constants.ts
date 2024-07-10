9168b8341a8b750e4f8ed0f2cd6705a2cb78d60b
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-10 21:30:44 +0100

    Clean up

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,10 +4,3 @@ export const CATEGORIES_URL = `${BASE_URL}/api/categories`;
 export const PRODUCT_URL = `${BASE_URL}/api/products`;
 export const UPLOAD_URL = `${BASE_URL}/api/uploads`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
-
-// export const BASE_URL = "";
-// export const USERS_URL = "/api/users";
-// export const CATEGORIES_URL = "/api/categories";
-// export const PRODUCT_URL = "/api/products";
-// export const UPLOAD_URL = "/api/uploads";
-// export const ORDERS_URL = "/api/orders";
