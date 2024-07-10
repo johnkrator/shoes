1c3052cc234ab2fe63b3b62f04fb6c28128ff587
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-10 21:23:52 +0100

    Route fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,13 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORIES_URL = "/api/categories";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/uploads";
-export const ORDERS_URL = "/api/orders";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://nextgen-shoes-backend.vercel.app";
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORIES_URL = `${BASE_URL}/api/categories`;
+export const PRODUCT_URL = `${BASE_URL}/api/products`;
+export const UPLOAD_URL = `${BASE_URL}/api/uploads`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+
+// export const BASE_URL = "";
+// export const USERS_URL = "/api/users";
+// export const CATEGORIES_URL = "/api/categories";
+// export const PRODUCT_URL = "/api/products";
+// export const UPLOAD_URL = "/api/uploads";
+// export const ORDERS_URL = "/api/orders";
