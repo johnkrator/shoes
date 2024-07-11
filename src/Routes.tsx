e93a565e00bb53e6dbb5841aa1b644493da95fab
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-11 15:23:03 +0100

    Update route path

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,7 +4,6 @@ import Layout from "@/Layout.tsx";
 import Home from "@/pages/Home.tsx";
 import Login from "@/pages/auth/Login.tsx";
 import Register from "@/pages/auth/Register.tsx";
-import About from "@/pages/About.tsx";
 import Contact from "@/pages/Contact.tsx";
 import Shop from "@/pages/Shop.tsx";
 import Cart from "@/pages/cart/Cart.tsx";
@@ -26,7 +25,6 @@ const Routes = () => {
                 {path: "", element: <Home/>},
                 {path: "login", element: <Login/>},
                 {path: "register", element: <Register/>},
-                {path: "about", element: <About/>},
                 {path: "contact", element: <Contact/>},
                 {path: "shop", element: <Shop/>},
                 {path: "cart", element: <Cart/>},
