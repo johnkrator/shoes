c2a72b671e2b2759ee81a6a2807b36c19bc09323
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-15 17:10:34 +0100

    Implement payment success page

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -2,6 +2,8 @@ import {useEffect} from "react";
 import Container from "@/Container.tsx";
 import confetti from "canvas-confetti";
 import {Button} from "@/components/ui/button.tsx";
+import {GoChevronLeft} from "react-icons/go";
+import {Link} from "react-router-dom";
 
 const PaymentSuccess = () => {
     useEffect(() => {
@@ -18,18 +20,24 @@ const PaymentSuccess = () => {
             <div className="flex flex-col items-center justify-center min-h-screen">
                 <h1 className="text-4xl text-center font-bold mb-4">Payment Successful!</h1>
                 <p className="text-xl text-gray-600">Thank you for your purchase.</p>
-                <Button
-                    className="mt-8 px-6 py-2 bg-[#e15420] text-white rounded-lg hover:bg-[#e15420] font-bold transition-colors"
-                    onClick={() => {
-                        confetti({
-                            particleCount: 100,
-                            spread: 70,
-                            origin: {y: 0.6}
-                        });
-                    }}
-                >
-                    Celebrate Again!
-                </Button>
+                <div className="flex flex-col gap-2">
+                    <Button
+                        className="mt-8 px-6 py-2 bg-[#e15420] text-white rounded-lg hover:bg-[#e15420] font-bold transition-colors"
+                        onClick={() => {
+                            confetti({
+                                particleCount: 100,
+                                spread: 70,
+                                origin: {y: 0.6}
+                            });
+                        }}
+                    >
+                        Celebrate Again!
+                    </Button>
+                    <Link className="flex items-center capitalize font-bold hover:underline" to="/">
+                        <GoChevronLeft/>
+                        go to back to homepage
+                    </Link>
+                </div>
             </div>
         </Container>
     );
