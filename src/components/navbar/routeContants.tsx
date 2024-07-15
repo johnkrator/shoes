e42464a05c3ab764b8e4b5859e08a3e2300e1222
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-15 10:46:21 +0100

    Refactor cart icon

--- src/components/navbar/routeContants.tsx
+++ src/components/navbar/routeContants.tsx
@@ -77,7 +77,7 @@ export const QueryItems: React.FC = () => {
                     <MdOutlineShoppingCart size={25}/>
                     {itemCount > 0 && (
                         <p
-                            className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                            className="absolute -top-2 -right-2 bg-[#e15420] font-bold text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
                             {itemCount}
                         </p>
                     )}
