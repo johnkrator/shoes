ddf5168917fbdc28a20d05b6cc8ca2231495e497
Chidiebere <chukwuchidieberejohn@gmail.com>
2024-07-30 08:22:50 +0100

    Add error handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import {RouterProvider} from "react-router-dom";
 import Routes from "@/Routes.tsx";
 import {ThemeProvider} from "@/components/theme-provider/ThemeProvider.tsx";
+import ErrorBoundary from "@/components/ErrorBoundary.tsx";
 
 const App = () => {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <RouterProvider router={Routes()}/>
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <RouterProvider router={Routes()}/>
+            </ThemeProvider>
+        </ErrorBoundary>
     );
 };
 
